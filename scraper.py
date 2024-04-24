b62e7f87e4fd6770f50374ef5ca901160e2d2242
patino209 <patinoc1@uci.edu>
2024-04-23 20:14:35 -0700

    separated some logic to functions

--- scraper.py
+++ scraper.py
@@ -56,44 +56,24 @@ def extract_next_links(url, resp):
 
     # Splitting URL into fragments, we only care about the defragged url
     defragged_url, throwaway = urldefrag(url)
-
     # Counting all unique URLs with the fragment cut off
     if defragged_url not in visited_defrags:
         visited_defrags.add(defragged_url)
 
 
-    # Soup object made out of current URL HTML content # TODO: check lxml
+    # Soup object made out of current URL HTML content
     soup = bs(resp.raw_response.content, 'lxml')
     text = soup.get_text()
     words = text.split()
     words = [word.lower() for word in words if word.isalnum()]
-    word_freq = computeWordFrequencies(words, stopwords)
-    for k, v in word_freq.items():
-        word_frequency[k] += v
 
-    # TODO: check validity
-    num_words = len(words)
-    if num_words > longest_info["longest_page_num"]:
-        longest_info["longest_page"] = url
-        longest_info["longest_page_num"] = num_words
+    word_freq = get_word_frequencies(words) # Generate map of tokens and their # of occurrences
+    populate_longest_page_info(url, words) # Update global longest_page_info map with info about longest page so far
+    populate_unique_subdomains(url) # Update global set of unique subdomains
 
-    try:
-        domains = urlparse(url).netloc.split(".")
-        subdomain = domains[:-3]
-        domain = domains[-3:]
-        if subdomain and "ics.uci.edu" in ".".join(domain):
-            subdomains.add("".join(subdomain))
-    except Exception as e:
-        pass
-    
-
-    # Compare the previous page to the current page using simhashing to check for similarity/duplicates
-    # Using a similarity percentage of 80% as the cutoff point
-    global prev_hash
-    sim_score = get_similarity_score(prev_hash, word_freq)
-    if prev_hash and sim_score > .8: # we choose .8 as our threshold here
+    # If simhash on two pages generates similarity score > 0.8
+    if is_too_similar(word_freq):
         return list()
-    prev_hash = word_freq
 
 
     # Avoid very large files, or traps, and avoid pages with low informational content
@@ -160,3 +140,37 @@ def is_valid(url):
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
+def populate_unique_subdomains(url: str):
+    # For a given url, check if its subdomain has been seen before
+    # add to set if never seen before
+    try:
+        domains = urlparse(url).netloc.split(".")
+        subdomain = domains[:-3]
+        domain = domains[-3:]
+        if subdomain and "ics.uci.edu" in ".".join(domain):
+            subdomains.add("".join(subdomain))
+    except Exception as e:
+        pass
+
+def populate_longest_page_info(url: str, words: str):
+    num_words = len(words)
+    if num_words > longest_info["longest_page_num"]:
+        longest_info["longest_page"] = url
+        longest_info["longest_page_num"] = num_words
+
+def get_word_frequencies(words: list):
+    word_freq = computeWordFrequencies(words, stopwords)
+    for k, v in word_freq.items():
+        word_frequency[k] += v
+
+def is_too_similar(word_freq: dict):
+    # Compare the previous page to the current page using simhashing to check for similarity/duplicates
+    # Using a similarity percentage of 80% as the cutoff point
+    global prev_hash
+    sim_score = get_similarity_score(prev_hash, word_freq)
+    if prev_hash and sim_score > .8: # we choose .8 as our threshold here
+        return True
+    prev_hash = word_freq
+
+    return False
