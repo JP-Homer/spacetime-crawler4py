21335033f52ac92c65097320168978d7c7e365c8
patino209 <patinoc1@uci.edu>
2024-04-29 17:49:21 -0700

    changed simhash to be 32 bit

--- simhash.py
+++ simhash.py
@@ -1,4 +1,5 @@
 from hashlib import md5
+bits = 32
 
 def simhash(token_map: dict) -> str:
     # Generates simhash fingerprint given a map of tokens and their weights (in our case, occurrences in a web page)
@@ -6,20 +7,20 @@ def simhash(token_map: dict) -> str:
     bit_map = dict()
 
     for word in token_map:
-        bit_map[word] = bin(0xFFFF & int(md5(word.encode()).hexdigest(), 16))[2:].zfill(16) # 16 least significant bits
+        bit_map[word] = bin(0xFFFF & int(md5(word.encode()).hexdigest(), bits))[2:].zfill(bits) # 16 least significant bits
     
-    vector = [0] * 16 # Vector with 16 zeroes
+    vector = [0] * bits # Vector with 16 zeroes
 
     # Looping through every word in the map {word -> binary}
     for k, v in bit_map.items():
         # Looping through every bit in the binary representation of a word
-        for i in range(16):
+        for i in range(bits):
             if(v[i] == "1"):
                 vector[i] += token_map[k]
             else:
                 vector[i] -= token_map[k]
 
-    for i in range(16):
+    for i in range(bits):
         # Creating final binary string for both vectors
         if vector[i] > 0:
             vector[i] = "1"
@@ -33,16 +34,16 @@ def get_similarity_score(fpA: str, fpB: str):
     # Given two fingerprints, generate a similarity score
     
     similarity_count = 0
-    for i in range(16):
+    for i in range(bits):
         if fpA[i] == fpB[i]: # if bit i is the same in both vectors
             similarity_count += 1
 
-    return similarity_count / 16 # number of same bits in the same place / 16 bits
+    return similarity_count / bits # number of same bits in the same place / 16 bits
 
 
 if __name__ == "__main__":
-    t1 = {"hello": 2, "goodbye": 1, "yellow": 3, "new": 5}
-    t2 = {"hello": 2, "yellow": 3}
+    t1 = {"this": 1, "is": 1, "the": 1, "first": 1, "string": 1}
+    t2 = {"this": 1, "is": 1, "the": 1, "second": 1, "string": 1}
 
     fingerprintA = simhash(t1)
     fingerprintB = simhash(t2)
